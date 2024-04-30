0ccb10ccf7dcf2f4921f78ed3474815a839f6a68
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2024-04-29 21:39:51 -0300

    básico de Property binding, Attribute binding, class and style binding, event binding, two-way binding, NgClass, Template variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,20 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NewComponent],
+  imports: [RouterOutlet, CommonModule, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
   template: `
   <!-- <router-outlet />  -->
   <h1>Hello</h1>
-  <h2>Components</h2>
   <app-new-component></app-new-component>
+
+  <app-template-binding/>
+  <app-template-variables/>
 
   `,
 })
---
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-binding',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './template-binding.component.html',
+  styleUrl: './template-binding.component.scss'
+})
+export class TemplateBindingComponent {
+  public name = 'Thomas Shelby'
+  public age = 32;
+  public isDisable = true
+  public isTextDecoration = this.age >= 32 ? 'underline' : 'none';
+
+  public sum() {
+    return this.age++;
+  }
+
+  public sub() {
+    return this.age--;
+  }
+
+  public onKeyDown(event: Event) {
+    return console.log(event)
+  }
+
+  public onMouseEvent(event: MouseEvent) {
+    return console.log(event.clientX)
+  }
+}
