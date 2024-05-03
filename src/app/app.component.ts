e40d932841980fd7ef275e3b7376d039573ed959
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2024-05-02 23:41:27 -0300

    introdução ao NgIf e NgFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,28 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    NewComponent,
+    TemplateBindingComponent,
+    TemplateVariablesComponent,
+    TemplateControlFlowComponent,
+  ],
   template: `
-  <!-- <router-outlet />  -->
-  <h1>Hello</h1>
+    <!-- <router-outlet />  -->
+    <!-- <h1>Hello</h1>
   <app-new-component></app-new-component>
 
   <app-template-binding/>
-  <app-template-variables/>
+  <app-template-variables/> -->
 
+    <app-template-control-flow />
   `,
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
